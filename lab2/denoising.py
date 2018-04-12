89d68c92cc2a2cea2a23a8d1394e931b2aa5c9f7
Kevin Lin <kevinptt0323@gmail.com>
2018-04-12 12:24:14 +0800

    not require input-gt

--- lab2/denoising.py
+++ lab2/denoising.py
@@ -17,14 +17,17 @@ if __name__ == '__main__':
     parser.add_argument('--step', default=2400, type=int, help='number of steps')
     parser.add_argument('--lr', default=1e-2, type=float, help='learning rate')
     parser.add_argument('--input', default='images/noise_image.png', type=str, help='path to input')
-    parser.add_argument('--input-gt', default='images/noise_GT.png', type=str, help='path to input ground truth')
+    parser.add_argument('--input-gt', type=str, help='path to input ground truth')
     parser.add_argument('--no-cuda', action='store_true', help='disable cuda')
     parser.add_argument('--output', type=str, help='directory to output')
     args = parser.parse_args()
 
     use_cuda = not args.no_cuda and torch.cuda.is_available()
 
-    truth = np.array(Image.open(args.input_gt))
+    if args.input_gt:
+        truth = np.array(Image.open(args.input_gt))
+    else:
+        truth = None
     target = image_to_tensor(args.input)
     input_depth = 32
     input_size = torch.Size([1, input_depth] + list(target.shape[2:]))
@@ -34,7 +37,7 @@ if __name__ == '__main__':
         target = target.cuda()
         noise = noise.cuda()
 
-    net = SkipHourglass(input_depth, 3,
+    net = SkipHourglass(input_depth, target.shape[1],
                         #down_channels=[8, 16, 32, 64, 128],
                         #up_channels=[8, 16, 32, 64, 128],
                         #skip_channels=[0, 0, 0, 4, 4],
@@ -71,8 +74,9 @@ if __name__ == '__main__':
                 net_output = net_output.cpu()
             filename = None if args.output is None else '%s/%04d.png' % (args.output, step)
             img = tensor_to_image(net_output.data, filename)
-            psnr = compare_psnr(truth, np.array(img))
-            max_psnr = max(max_psnr, psnr)
+            if truth:
+                psnr = compare_psnr(truth, np.array(img))
+                max_psnr = max(max_psnr, psnr)
 
         progress.set_description('Loss: %.6f | PSNR: %.2f dB | Max PSNR: %.2f dB'
             % (train_loss, psnr, max_psnr))
